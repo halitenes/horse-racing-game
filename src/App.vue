a9679a7d4239efe06b7a83730e389c679b8eabd8
halitenes <buyuktepe.halitenes@gmail.com>
2026-04-09 15:35:00 +0300

    feat: add basic app layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,76 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div class="app">
+    <header class="header">
+      <h1>Horse Racing</h1>
+      <div class="header-buttons">
+        <button
+          class="btn btn-primary"
+          @click="generateProgram"
+          :disabled="isRacing"
+        >
+          GENERATE PROGRAM
+        </button>
+        <button
+          class="btn btn-secondary"
+          @click="toggleRace"
+          :disabled="!hasSchedule"
+        >
+          {{ raceButtonText }}
+        </button>
+      </div>
+    </header>
+
+    <main class="main-content">
+      <HorseList />
+      <RaceTrack />
+      <ProgramPanel />
+      <ResultsPanel />
+    </main>
+  </div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import HorseList from './components/HorseList.vue'
+import RaceTrack from './components/RaceTrack.vue'
+import ProgramPanel from './components/ProgramPanel.vue'
+import ResultsPanel from './components/ResultsPanel.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HorseList,
+    RaceTrack,
+    ProgramPanel,
+    ResultsPanel
+  },
+  computed: {
+    ...mapGetters(['hasSchedule', 'isRacing', 'isPaused', 'isFinished']),
+    raceButtonText() {
+      if (this.isFinished) return 'FINISHED'
+      if (this.isPaused) return 'RESUME'
+      if (this.isRacing) return 'PAUSE'
+      return 'START'
+    }
+  },
+  methods: {
+    ...mapActions(['generateHorses', 'generateSchedule', 'startRace', 'pauseRace', 'resumeRace']),
+    generateProgram() {
+      this.generateHorses()
+      this.generateSchedule()
+    },
+    toggleRace() {
+      if (this.isFinished) return
+      if (this.isRacing) {
+        this.pauseRace()
+      } else {
+        if (this.isPaused) {
+          this.resumeRace()
+        } else {
+          this.startRace()
+        }
+      }
+    }
+  }
+}
+</script>
